6914f72f5a63ddd924b7171e9ac1820a0005cf56
Cao Mai <caomai@rocketmail.com>
2020-01-22 15:36:54 -0800

    added search form

--- app.js
+++ app.js
@@ -15,15 +15,16 @@ app.set('view engine', 'handlebars');
 // Routes
 // Routes
 app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
+    // const term = req.params.term;
+    const query = req.query;
+    const term = query['term']
+
+    res.render('home', { term })
   })
 
 app.get('/greetings/:name', (req, res) => {
   // grab the name from the path provided
-  const name = req.params.name;
+  
   // render the greetings view, passing along the name
   res.render('greetings', { name });
 })
